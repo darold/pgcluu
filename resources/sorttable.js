83fc821dd21b303d515896eb3faa30a0e8d025be
Darold Gilles <gilles@darold.net>
2015-07-26 15:15:25 +0200

    Update CSS and JS files in resources directory and datetimepicker ressources files for cgi.

--- resources/sorttable.js
+++ resources/sorttable.js
@@ -1,4 +1,7 @@
 /*
+  Modified version of sorttable for pgCluu.
+*/
+/*
   SortTable
   version 2
   7th April 2007
@@ -151,7 +154,7 @@ sorttable = {
 	        //sorttable.shaker_sort(row_array, this.sorttable_sortfunction);
 	        /* and comment out this one */
 	        row_array.sort(this.sorttable_sortfunction);
-		// SquidAnalyzer: Sort in descending order first
+		// pgCluu: Sort in descending order first
 		row_array.reverse();
 
 	        tb = this.sorttable_tbody;
@@ -171,7 +174,7 @@ sorttable = {
     for (var i=0; i<table.tBodies[0].rows.length; i++) {
       text = sorttable.getInnerText(table.tBodies[0].rows[i].cells[column]);
       if (text != '') {
-        if (text.match(/^-?[£$¤]?[\d,.]+%?$/)) {
+        if (text.match(/^-?[Â£$Â¤]?[\d,.]+\s*[%KMGTP]?[B]?$/)) {
           return sorttable.sort_numeric;
         }
         // check for a date: dd/mm/yyyy or dd/mm/yy
@@ -261,14 +264,41 @@ sorttable = {
      each sort function takes two parameters, a and b
      you are comparing a[0] and b[0] */
   sort_numeric: function(a,b) {
+    am = 1;
+    if (a[0].match(/KB/)) {
+	am = 1000;
+    } else if (a[0].match(/MB/)) {
+	am = 1000000;
+    } else if (a[0].match(/GB/)) {
+	am = 1000000000;
+    } else if (a[0].match(/TB/)) {
+	am = 1000000000000;
+    } else if (a[0].match(/PB/)) {
+	am = 1000000000000000;
+    }
     aa = parseFloat(a[0].replace(/[^0-9.-]/g,''));
     if (isNaN(aa)) aa = 0;
+    aa = aa*am;
+    bm = 1;
+    if (b[0].match(/KB/)) {
+	bm = 1000;
+    } else if (b[0].match(/MB/)) {
+	bm = 1000000;
+    } else if (b[0].match(/GB/)) {
+	bm = 1000000000;
+    } else if (b[0].match(/TB/)) {
+	bm = 1000000000000;
+    } else if (b[0].match(/PB/)) {
+	bm = 1000000000000000;
+    }
     bb = parseFloat(b[0].replace(/[^0-9.-]/g,''));
     if (isNaN(bb)) bb = 0;
+    bb = bb*bm;
+
     return aa-bb;
   },
   sort_alpha: function(a,b) {
-    // SquidAnalyzer: remove percentage for numeric sort
+    // PgCluu: remove percentage for numeric sort
     if (a[0].replace(/ <.*\(.*%\).*/, '')) {
 	b[0].replace(/ <.*\(.*%\).*/,''); 
 	aa = parseFloat(a[0].replace(/[^0-9.-]/g,''));
